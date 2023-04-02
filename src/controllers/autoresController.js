581479db746a2c75a4ca85e62af8545650438315
AshileySabah <ashileysabrina2002@gmail.com>
2023-04-02 15:49:28 -0300

    refactor: error messages

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -5,49 +6,56 @@ class AutorController {
     try{
       const autoresResultado = await autores.find();
       res.status(200).json(autoresResultado);
-    }catch(err){
+    }catch(erro){
       res.status(500).json({ message: "Erro interno no servidor." });
     }
   };
 
   static getAutorById = async (req, res) => {
     try{
-      const id = req?.params?.id;
+      const id = req.params.id;
       const autorResultado = await autores.findById(id);
-      res.status(200).send(autorResultado);
-    }catch(err){
-      res.status(400).send({ message: "Autor não existente." });
+      if (autorResultado !== null) {
+        res.status(200).send(autorResultado);
+      } else {
+        res.status(404).send({ message: "Id do Autor não localizado." });
+      }
+    }catch(erro){
+      if (erro instanceof mongoose.Error.CastError) {
+        res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos." });
+      } else {
+        res.status(500).send({ message: "Erro interno de servidor." });
+      }
     }
-   
   };
 
   static createAutor = async (req, res) => {
     try{
-      const autor = new autores(req?.body);
-      await autor.save();
-      res.status(201).send(autor.toJSON());
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao cadastrar autor.` });
+      const autor = new autores(req.body);
+      const autorResultado = await autor.save();
+      res.status(201).send(autorResultado.toJSON());
+    }catch(erro){
+      res.status(500).send({ message: `${erro.message} - falha ao cadastrar autor.` });
     }
   };
 
   static updateAutor = async (req, res) => {
     try{
-      const id = req?.params?.id;
+      const id = req.params.id;
       await autores.findByIdAndUpdate(id, { $set: req.body });
       res.status(200).send({ message: "Autor atualizado com sucesso." });
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao atualizar autor.` });
+    }catch(erro){
+      res.status(500).send({ message: `${erro.message} - falha ao atualizar autor.` });
     }
   };
 
   static deleteAutor = async (req, res) => {
     try{
-      const id = req?.params?.id;
+      const id = req.params.id;
       await autores.findByIdAndDelete(id);
       res.status(200).send({ message: "Autor deletado com sucesso." });
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao deletar autor.` });
+    }catch(erro){
+      res.status(500).send({ message: erro.message });
     }
   };
 }
--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,62 +1,71 @@
+import mongoose from "mongoose";
 import livros from "../models/Livro.js";
 
 class LivroController {
   static getAllLivros = async (req, res) => {
     try{
-      const livrosResultado = await livros.find().populate("autor").exec();
+      const livrosResultado = await livros.find();
       res.status(200).json(livrosResultado);
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao cadastrar livro.` });
+    }catch(erro){
+      res.status(500).json({ message: "Erro interno no servidor." });
     }
   };
 
   static getLivroById = async (req, res) => {
     try{
-      const id = req?.params?.id;
-      const livroResultado = await livros.findById(id).populate("autor", "nome").exec();
-      res.status(200).send(livroResultado.toJSON());
-    }catch(err){
-      res.status(400).send({ message: "Livro não existente." });
+      const id = req.params.id;
+      const livroResultado = await livros.findById(id);
+      if (livroResultado !== null) {
+        res.status(200).send(livroResultado);
+      } else {
+        res.status(404).send({ message: "Id do Livro não localizado." });
+      }
+    }catch(erro){
+      if (erro instanceof mongoose.Error.CastError) {
+        res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos." });
+      } else {
+        res.status(500).send({ message: "Erro interno de servidor." });
+      }
     }
   };
 
   static createLivro = async (req, res) => {
     try{
-      const livro = new livros(req?.body);
-      await livro.save();
-      res.status(201).send(livro.toJSON());
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao cadastrar livro.` });
+      const livro = new livros(req.body);
+      const livroResultado = await livro.save();
+      res.status(201).send(livroResultado.toJSON());
+    }catch(erro){
+      res.status(500).send({ message: `${erro.message} - falha ao cadastrar livro.` });
     }
   };
 
   static updateLivro = async (req, res) => {
     try{
-      const id = req?.params?.id;
+      const id = req.params.id;
       await livros.findByIdAndUpdate(id, { $set: req.body });
       res.status(200).send({ message: "Livro atualizado com sucesso." });
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao atualizar livro.` });
+    }catch(erro){
+      res.status(500).send({ message: `${erro.message} - falha ao atualizar livro.` });
     }
   };
 
   static deleteLivro = async (req, res) => {
     try{
-      const id = req?.params?.id;
+      const id = req.params.id;
       await livros.findByIdAndDelete(id);
       res.status(200).send({ message: "Livro deletado com sucesso." });
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao deletar livro.` });
+    }catch(erro){
+      res.status(500).send({ message: erro.message });
     }
   };
 
   static getLivrosByEditora = async (req, res) => {
     try{
-      const { editora } = req.query;
-      const livrosResultado = await livros.find({ "editora": editora }, {}, );
-      res.status(200).json(livrosResultado);
-    }catch(err){
-      res.status(500).send({ message: `${err.message} - falha ao encontrar livros.` });
+      const editora = req.query.editora;
+      const livrosResultado = await livros.find({ "editora": editora });
+      res.status(200).send(livrosResultado);
+    }catch(erro){
+      res.status(500).json({ message: "Erro interno no servidor" });
     }
   };
 }
